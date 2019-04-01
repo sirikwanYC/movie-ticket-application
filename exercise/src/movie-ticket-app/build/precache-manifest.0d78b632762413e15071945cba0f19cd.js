6803a902efaa3a78993ebdcbeb8bbff682f14a77
sirikwanYC <sirikwan_yanchan@hotmail.com>
2019-04-01 22:51:09 +0700

    upload to host

---
+++ exercise/src/movie-ticket-app/build/precache-manifest.0d78b632762413e15071945cba0f19cd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f8d071df90ce9c4ded65",
+    "url": "/static/js/main.845273e9.chunk.js"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "5d2571261516b57930ce55c9cf03ffc6",
+    "url": "/index.html"
+  }
+];
